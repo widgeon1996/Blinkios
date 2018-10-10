1d38546a14dfcaff5f4466b9ffae763fc2a8cd55
Ryan <widgeon1996@ufl.edu>
2018-10-09 23:29:47 -0400

    Initial Commit

---
+++ Bl!nk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bl!nk
+//
+//  Created by Ryan Widgeon on 10/9/18.
+//  Copyright © 2018 Ryan Widgeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
