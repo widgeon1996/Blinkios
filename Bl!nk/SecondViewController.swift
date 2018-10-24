00ea022f47f99c83116cd58d509c17e97e9b4ad5
Ryan <widgeon1996@ufl.edu>
2018-10-24 16:52:04 -0400

    Initial commit

---
+++ Bl!nk/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Bl!nk
+//
+//  Created by Ryan Widgeon on 10/22/18.
+//  Copyright © 2018 Ryan Widgeon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Bl!nk/colors.swift
@@ -0,0 +1,9 @@
+//
+//  colors.swift
+//  Bl!nk
+//
+//  Created by Ryan Widgeon on 10/24/18.
+//  Copyright © 2018 Ryan Widgeon. All rights reserved.
+//
+
+import Foundation
